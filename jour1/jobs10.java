4ab2f3cea2c8fd9d0b4ce4876452320fa24f207f
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-30 14:10:59 +0200

    rajout du jobs 10

---
+++ jour1/jobs10.java
@@ -0,0 +1,10 @@
+public class jobs10 {
+    public static void main(String[] args) {
+        int factorial = 1;
+        for(int i=1; i<=8; i++){
+            factorial *= i;
+        }
+        System.out.println("La factorielle de 8 est : " + factorial);
+    }
+    
+}
