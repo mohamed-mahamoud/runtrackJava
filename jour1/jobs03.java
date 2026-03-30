a8adc677d993ed9792d31b645fb2a3eadd5106db
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-30 12:22:27 +0200

    rajout du jobs 03

---
+++ jour1/jobs03.java
@@ -0,0 +1,10 @@
+public class jobs03 {
+    public static void main(String[] args){
+        int tab[] = {0,1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("tab[0] = " + tab[0]);
+        System.out.println("tab[1] = " + tab[1]);
+        System.out.println("tab[5] = " + tab[5]);
+        System.out.println("tab[9] = " + tab[9]);
+        System.out.println("tab[10] = " + tab[10]);
+    }
+}
