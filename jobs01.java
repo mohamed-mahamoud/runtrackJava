13a3958c38b560e2c6982ea827bbae190ae28eb5
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-30 11:30:11 +0200

    jobs 01 fini

---
+++ jobs01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class jobs01 {
+    public static void main(String[] args) {
+        String mot;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ecriver un mot");
+        mot=scan.next();
+        System.out.println("Vous avez ecrit: " + mot);
+    }
+}
