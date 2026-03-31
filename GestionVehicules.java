7cec7a7494bbedcb4e9f8022c870546096c8ac7d
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-31 10:54:16 +0200

    rajout de la deuxieme partie du jobs 04

--- GestionVehicules.java
+++ GestionVehicules.java
@@ -16,6 +16,38 @@ public class GestionVehicules {
             System.out.println("Prix d'achat : " + prixAchat);
         }
     }
-
-    
+        class Voiture extends Vehicule {
+            protected String cylindre;
+            protected int nombrePortes;
+            protected double puissance;
+            protected double kilometrage;
+            public Voiture(String marque, String dateAchat, double prixAchat, String cylindre, int nombrePortes, double puissance, double kilometrage) {
+                super(marque, dateAchat, prixAchat);
+                this.cylindre = cylindre;
+                this.nombrePortes = nombrePortes;
+                this.puissance = puissance;
+                this.kilometrage = kilometrage;
+            }
+            public void affiche(){
+                super.affiche();
+                System.out.println("Cylindre : " + cylindre);
+                System.out.println("Nombre de portes : " + nombrePortes);
+                System.out.println("Puissance : " + puissance);
+                System.out.println("Kilometrage : " + kilometrage);
+            }
+    }
+        class Avion extends Vehicule {
+            protected String moteur;
+            protected double heuresVol;
+            public Avion(String marque, String dateAchat, double prixAchat, String moteur, double heuresVol) {
+                super(marque, dateAchat, prixAchat);
+                this.moteur = moteur;
+                this.heuresVol = heuresVol;
+            }
+            public void affiche(){
+                super.affiche();
+                System.out.println("Moteur : " + moteur);
+                System.out.println("Heures de vol : " + heuresVol);
+            }
+        }
 }
