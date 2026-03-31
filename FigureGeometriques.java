72e12f69457bbf63ea291e264f12558a428147b4
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-31 10:35:39 +0200

    rajout de la de la seconde partie du jobs 03

--- FigureGeometriques.java
+++ FigureGeometriques.java
@@ -22,5 +22,46 @@ public class FigureGeometriques {
             this.largeur = largeur;
         }
     }
-    
+    class Cercle{
+        protected double x;
+        protected double y;
+        protected double rayon;
+        public Cercle(double x, double y, double rayon) {
+            this.x = x;
+            this.y = y;
+            this.rayon = rayon;
+        }
+        public void affiche(){
+            System.out.println("Cercle de centre (" + x + ", " + y + ") et de rayon " + rayon);
+        }
+        public double getX() {
+            return x;
+        }
+        public double getY() {
+            return y;
+        }
+        public double getRayon() {
+            return rayon;
+        }
+        public void setX(double x) {
+            this.x = x;
+        }
+        public void setY(double y) {
+            this.y = y;
+        }
+        public void setRayon(double rayon) {
+            this.rayon = rayon;
+        }
+        public void setCentre(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+        public double surface() {
+            return Math.PI * rayon * rayon;
+        }
+        public boolean estInterieur(double x, double y) {
+            double distance = Math.sqrt(Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2));
+            return distance < rayon;
+        }
+    }
 }
