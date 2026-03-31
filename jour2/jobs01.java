ed8588677379d0988180a80db6fb9008d6a0aa53
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-31 10:19:02 +0200

    rajout jour 2 avec jobs 1 et 2

---
+++ jour2/jobs01.java
@@ -0,0 +1,19 @@
+
+
+class Toto{
+    int toto =0;
+    Toto(){
+        toto=toto+1;
+
+    }
+    public static void main(String[] tutu) {
+        Toto t1 = new Toto();
+        Toto t2 = new Toto();
+        System.out.println("Toto :"+toto);
+        
+    }
+}
+
+//jobs01.java:12: error: non-static variable toto cannot be referenced from a static context
+//       System.out.println("Toto :"+toto);
+                                    ^
---
+++ jour2/jobs02.java
@@ -0,0 +1,34 @@
+class jobs02 {
+    public static void main(String[] args) {
+        E1 x = new E1();
+        E2 y = new E2();
+        E3 z = new E3();
+        E4 v = new E4();
+        E5 w = new E5();
+        System.out.println(x.a); //Corect
+        System.out.println(y.b); // faux
+        System.out.println(z.b); //corect
+        System.out.println(v.d); //correct
+        System.out.println(w.a); //faux   
+    }
+}
+
+class E1 {
+    int a = 1;
+}
+
+class E2 extends E1 {
+    int b = 2;
+}
+
+class E3 extends E2 {
+    int c = 3;
+}
+
+class E4 extends E1 {
+    int d = 4;
+}
+
+class E5 extends E4 {
+    int e = 5;
+}
