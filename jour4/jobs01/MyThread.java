418de8ab8ba3d4a4cc8b90924e6d35f62b5e4d0c
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-04-02 11:20:17 +0200

    rajout du jour 4 et jobs 01 fini

---
+++ jour4/jobs01/MyThread.java
@@ -0,0 +1,9 @@
+public class MyThread extends Thread {
+    
+    @Override
+    public void run() {
+        System.out.println("Mon thred est en cours ");
+
+    }
+
+}
---
+++ jour4/jobs01/ThreadProject.java
@@ -0,0 +1,7 @@
+public class ThreadProject {
+
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+}
